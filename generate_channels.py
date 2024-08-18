a454a72afd2c60addada01d59579141806717d23
RonaldoLionelLevandovsky <yevgen_hryshko@outlook.com>
2024-08-18 13:13:09 +0300

    Update generate_channels.py

--- generate_channels.py
+++ generate_channels.py
@@ -35,7 +35,7 @@ def save_unique_channels(unique_channels, output_file):
 
 base_directory = 'sites'
 output_file = 'updated_channels.xml'
-target_dirs = ['tv.cctv.com','startimestv.com']
+target_dirs = ['tv.cctv.com']
 
 unique_channels = collect_unique_channels(base_directory, target_dirs)
 save_unique_channels(unique_channels, output_file)
